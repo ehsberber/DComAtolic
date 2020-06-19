9ce79d50288d8c17a33679799d705362a06e6294
ehsberber <sberber@masterdecos.com>
2020-06-19 17:47:44 +0300

    commit 4

--- DComAtalic/Src/main.c
+++ DComAtalic/Src/main.c
@@ -65,7 +65,7 @@ void systemInit(void);
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
 //TODO SPI values must be configurative from user config
-//TODO Commit 3
+//TODO Test Commit 4
 //Variable Declerations
 unsigned char ucaPLCUartTxData[PLC_UART_TX_BUFFER_LENGHT];
 unsigned char ucaPLCUartRxData[PLC_UART_RX_BUFFER_LENGHT];
